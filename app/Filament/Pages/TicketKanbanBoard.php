8c94f4b25b79fb6ec5f599fb4358434b9b12c1ee
Amrit Tamang <amttmg@gmail.com>
2025-05-15 16:12:18 +0545

    qqqq

--- app/Filament/Pages/TicketKanbanBoard.php
+++ app/Filament/Pages/TicketKanbanBoard.php
@@ -12,4 +12,15 @@ class TicketKanbanBoard extends KanbanBoard
     protected static string $model = Ticket::class;
     protected static string $statusEnum = TicketStatus::class;
     protected static string $recordStatusAttribute = 'status_id';
+    protected static string $kanbanHeight = '1000px';
+
+    protected function records(): Collection
+    {
+        return Ticket::query()
+            ->with(['status'])
+            ->whereHas('agents', function ($query) {
+                $query->where('users.id', auth()->id());
+            })
+            ->get();
+    }
 }
