17c72540275f33cecb53f5a0d96d9c6e849f3dd7
Amrit Tamang <amttmg@gmail.com>
2025-05-23 13:12:46 +0545

    tickets

--- resources/js/Components/TicketTabs.vue
+++ resources/js/Components/TicketTabs.vue
@@ -14,12 +14,12 @@
                 'text-blue-600 border-b-2 border-blue-500': active === 'my-tickets',
                 'text-gray-500 hover:text-gray-700': active !== 'my-tickets'
             }">
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline w-4 h-4 mr-1" :class="{
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" :class="{
                     'text-blue-500': active === 'my-tickets',
                     'text-gray-400': active !== 'my-tickets'
                 }" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
         </svg>
         My Tickets
         </Link>
@@ -29,12 +29,12 @@
                 'text-blue-600 border-b-2 border-blue-500': active === 'branch-tickets',
                 'text-gray-500 hover:text-gray-700': active !== 'branch-tickets'
             }">
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline w-4 h-4 mr-1" :class="{
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" :class="{
                     'text-blue-500': active === 'branch-tickets',
                     'text-gray-400': active !== 'branch-tickets'
                 }" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M3 10l9-7 9 7M4 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10" />
+                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
         </svg>
         Branch Tickets
         </Link>
