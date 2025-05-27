f014de734b5411290542b3623a10cde60476159c
Amrit Tamang <amttmg@gmail.com>
2025-05-27 07:19:32 +0545

    design change

--- resources/views/vendor/filament-kanban/kanban-status.blade.php
+++ resources/views/vendor/filament-kanban/kanban-status.blade.php
@@ -1,10 +1,11 @@
 @props(['status'])
 
-<div class="md:w-[24rem] flex-shrink-0 mb-5 md:min-h-full flex flex-col" style="min-height: 450px">
+<div class="md:w-[24rem] flex-shrink-0 mb-5 flex flex-col min-h-[80vh] md:min-h-[65vh]">
     @include(static::$headerView)
 
     <div data-status-id="{{ $status['id'] }}"
-        class="flex flex-col flex-1 gap-2 p-3 bg-gray-200 dark:bg-gray-800 rounded-xl" style="min-height: 100px">
+        class="flex flex-col flex-1 gap-2 p-3 overflow-y-auto bg-gray-200 dark:bg-gray-800 rounded-xl"
+        style="max-height: calc(100vh - 300px); min-height: calc(100vh - 300px);">
         @foreach ($status['records'] as $record)
             @include(static::$recordView)
         @endforeach
